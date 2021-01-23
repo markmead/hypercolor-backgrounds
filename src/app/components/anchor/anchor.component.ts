7fb4a573b94727abdbd0e95544f6b5d3267ed0e4
markmead <itsmarkmead@gmail.com>
2021-01-23 17:09:25 +0000

    seo tweaks

--- src/app/components/anchor/anchor.component.ts
+++ src/app/components/anchor/anchor.component.ts
@@ -2,8 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-anchor',
-  templateUrl: './anchor.component.html',
   styleUrls: ['./anchor.component.scss'],
+  template: `
+    <a
+      href="#{{ title }}"
+      class="relative flex-shrink-0 block m-1 transition-all duration-300 transform rounded-lg focus:outline-none focus:ring hover:shadow-xl hover:z-10"
+      [ngClass]="[colors, size]"
+      [title]="title"
+    >
+    </a>
+  `
 })
 export class AnchorComponent {
   @Input() colors!: string;
